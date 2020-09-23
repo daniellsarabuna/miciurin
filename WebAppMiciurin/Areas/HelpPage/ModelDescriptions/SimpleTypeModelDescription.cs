707e52a8dcee184cfaf47151fe2ce35d0d5e1e76
Daniel <daniell.sarabuna@gmail.com>
2020-09-23 20:28:30 +0300

    v1.0

---
+++ WebAppMiciurin/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace WebAppMiciurin.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ WebAppMiciurin/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebAppMiciurin.MvcApplication" Language="C#" %>
